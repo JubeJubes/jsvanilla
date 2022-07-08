36a20b4486b3bd45a66e09cf93b3cfdbd93a4130
JubeJubes <jubejubesgit@gmail.com>
2022-07-07 23:53:34 -0400

    color flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener("click",colorChange)
+
+function colorChange() {
+    color.textContent =  document.body.style.backgroundColor = colors[Math.floor(Math.random()*4)]
+    
+}
+
+
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,14 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener("click",()=>{
+    let cstring ='#'
+    for (let i=0;i<6;i++) {
+        cstring = cstring.concat(hex[Math.floor(Math.random()*hex.length)])
+    }
+    document.body.style.background = cstring;
+    color.textContent  = color.style.color = cstring
+    
+})
